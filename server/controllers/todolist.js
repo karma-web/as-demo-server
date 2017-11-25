9f5abdfa90ac18786c6955132a71e2bf0263e626
karma <lixiaoning@t2cloud.net>
2017-11-25 16:54:24 +0800

    优化代码

--- server/controllers/todolist.js
+++ server/controllers/todolist.js
@@ -1,16 +1,16 @@
 import todolist from '../models/todolist'
 
-const getTodolist = async function (ctx){ // 获取某个用户的所有todolist
+const getTodolist = async function (ctx){ // 获取列表
     const id = ctx.params.id;
     const result = await todolist.getTodolistById(id);
     ctx.body = result
 }
-const createTodolist = async function (ctx){ // 创建一条todolist
+const createTodolist = async function (ctx){ // 创建
     const data = ctx.request.body;
     const result = await todolist.createTodolist(data);
     ctx.body = result
 }
-const removeTodolist = async function (ctx){ // 删除一条todolist
+const removeTodolist = async function (ctx){ // 删除
     const id = ctx.params.id
     const userId = ctx.params.userId
     await todolist.removeTodolist(id, userId)
@@ -18,13 +18,13 @@ const removeTodolist = async function (ctx){ // 删除一条todolist
         success: true
     }
 }
-const updateTodolist = async function (ctx){ // 更新一条todolist
+const updateTodolist = async function (ctx){ // 更新
     const id = ctx.params.id
     const userId = ctx.params.userId
     const data = ctx.request.body;
     let favorite = data.favorite
     let text = data.text
-    favorite === '0' ? favorite = true : favorite = false
+    favorite === '0' ? favorite = false : favorite = true
     await todolist.updateTodolist(id, userId, favorite, text)
     ctx.body = {
         success: true
--- server/models/todolist.js
+++ server/models/todolist.js
@@ -1,11 +1,11 @@
-import db from '../config/db.js' // 引入todolist的表结构
+import db from '../config/db.js' // 引入表结构
 const todoModel = '../schema/list.js'
 const TodolistDb = db.Todolist // 引入数据库
 
 const Todolist = TodolistDb.import(todoModel) // 用sequelize的import方法引入表结构，Todolist。
 
 const getTodolistById = async function (id) {
-    const todolist = await Todolist.findAll({ // 查找全部的todolist
+    const todolist = await Todolist.findAll({ // 查找全部的数据
         where: {
             user_id: id
         },
@@ -15,10 +15,10 @@ const getTodolistById = async function (id) {
         ]
     })
 
-    return todolist // 返回数据
+    return todolist
 }
 
-const createTodolist = async function (data) { // 添加一条todolist
+const createTodolist = async function (data) { // 添加
     const newData = await Todolist.create({
         user_id: data.id,
         text: data.text,
@@ -27,7 +27,7 @@ const createTodolist = async function (data) { // 添加一条todolist
     return newData
 }
 
-const removeTodolist = async function (id,userId) { //删除一条todolist
+const removeTodolist = async function (id,userId) { //删除
     await Todolist.destroy({
         where: {
             id,
@@ -37,7 +37,7 @@ const removeTodolist = async function (id,userId) { //删除一条todolist
     return true
 }
 
-const updateTodolist = async function (id, userId, favorite, text) { //更新一条todolist
+const updateTodolist = async function (id, userId, favorite, text) { //更新
     await Todolist.update(
         {
             favorite,
